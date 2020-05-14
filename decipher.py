04d5fbde2d57071d27b2bff744eb416180f16204
witcher42 <junelee@student.42seoul.kr>
2020-05-14 17:11:43 +0900

    ground 5

--- decipher.py
+++ decipher.py
@@ -18,7 +18,7 @@ def extEuclid(a, b):
     return s0, t0, a 			# a: always 1
 
 def sqrRoot(n, q):
-    r = pow(n,(q+1)/4,q) 		#r is a quadratic residue.
+    r = pow(n,(q+1)/4,q) 		# r is a quadratic residue
     return r, q - r
 
 Point = collections.namedtuple("Point", ["x", "y"])
@@ -27,7 +27,7 @@ class EC(object):
     def __init__(self, a, b, q):
         assert 0 < a and a < q and 0 < b and b < q and q > 2
         assert (4 * (a ** 3) + 27 * (b ** 2))  % q != 0 
-        self.a = a			# no middle root. (mod q)
+        self.a = a			# no middle root (mod q)
         self.b = b
         self.q = q
         self.zero = Point(0, 0)
@@ -55,7 +55,7 @@ class EC(object):
         return Point(x, y)
 
     def smul(self, P, n):
-        r = self.zero 			# Point(0, 0). init every smul
+        r = self.zero 			# Point(0, 0) : init every smul
         m2 = P
         while 0 < n: 			# O(log(n))
             if n & 1 == 1: 		# last try
@@ -111,7 +111,7 @@ class STREAM():
 
     def decryption(self, known_pt):	# attackers_dec(known_pt)
 	r = self.ec.random(0)		# original__dec(ct, private_key)	
-	G = self.ec.random(r.x)		# start point G. order of the G is q.
+	G = self.ec.random(r.x)		# start point G : order of the G is q
 	private_key = G.x		# assume (!) change G.x
 	public__key = self.ec.smul(G,private_key)
 	validation, y = ec.findY(G.x)
@@ -136,14 +136,14 @@ class STREAM():
 	    pt = stream.encryption(bytearray(rt)) 
 	    if (pt.find(kpt) != 0): 	# -1
 	    	return pt
-	    self.seed-=1
+	    self.seed+=1
 
 
 if __name__ == "__main__":
     prime = 112817876910624391112586233842848268584935393852332056135638763933471640076719
     A = 49606376303929463253586154769489869489108883753251757521607397128446713725753
     B = 79746959374671415610195463996521688925529471350164217787900499181173830926217
-    ec = EC(A,B,prime) 			# A, B, prime : open to public.
+    ec = EC(A,B,prime) 			# A, B, prime : open to public
     P = Point(103039657693294116462834651854367833897272806854412839639851017006923575559024,
               77619251402197618012332577948300478225863306465872072566919796455982120391100)
     Q = Point(54754931428196528902595765731417656438047316294230479980073352787194748472682,
